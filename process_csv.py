46f2b4353132cc1f7bab5492f929706acf891313
ZonePG <zonepg666@gmail.com>
2023-01-16 15:36:14 +0800

    update process_csv.py

--- process_csv.py
+++ process_csv.py
@@ -3,19 +3,19 @@ import pandas as pd
 import re
 import csv
 import openpyxl
-path = "./result/INSGA2/"
+from glob import glob
+path = './result/INSGA2'
 
 # 打开一个新的 Excel 文件
 workbook = openpyxl.Workbook()
 
-namelist = sorted(os.listdir(path), key=lambda f: int(re.sub('\D', '', f)))
-
+namelist = sorted(glob(os.path.join(path, 'data_*.csv')), key=lambda f: int(re.sub('\D', '', f)))
 # namelist = ['data_2722.csv']
 
 for file_name in namelist:
-    instanceNo = file_name.split('.', 1)[0]
+    instanceNo = file_name.split('/')[-1][:-4]
     print(instanceNo)
-    with open(path + "/" + file_name, 'r', encoding='utf-8') as csvfile:
+    with open(file_name, 'r', encoding='utf-8') as csvfile:
         sheet = workbook.create_sheet(instanceNo)
         # 读取 csv 文件中的数据
         reader = csv.reader(csvfile)
