2040b85ab5b52cff9ec2d676eb19614a6112a31d
Zhiaan <zhiaan@foxmail.com>
2023-01-17 13:35:20 +0800

    update process_csv.py

--- process_csv.py
+++ process_csv.py
@@ -3,6 +3,7 @@ import pandas as pd
 import re
 import csv
 import openpyxl
+from openpyxl.styles import Font
 from glob import glob
 path = './result/INSGA2'
 
@@ -19,10 +20,22 @@ for file_name in namelist:
         sheet = workbook.create_sheet(instanceNo)
         # 读取 csv 文件中的数据
         reader = csv.reader(csvfile)
+
+        carNum = 0
         # 将数据写入 sheet1
         for row in reader:
             sheet.append(row)
+            carNum = len(row) - 3
+        for j in range(1, carNum + 4):
+            cell = sheet.cell(row=1, column=j)
+            cell.font = Font(name="等线 (正文)", size=11)
+        for i in range(2, 52):
+            for j in range(1, carNum + 4):
+                cell = sheet.cell(row=i, column=j)
+                cell.data_type = "int"
+                cell.font = Font(name="等线 (正文)", size=11)
 
+del workbook["Sheet"]
 # 保存 Excel 文件
 workbook.save('./result/result.xlsx')
 print('OK!')
