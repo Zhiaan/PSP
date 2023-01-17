6386b29bbf69c19445e7ed5aa9803b430dd1fbad
Zhiaan <zhiaan@foxmail.com>
2023-01-17 20:45:59 +0800

    update Param.cpp

--- src/comm/Param.cpp
+++ src/comm/Param.cpp
@@ -10,10 +10,9 @@ Param::Param(vector<string>& arguments) {
     problemName = "PSP";
     algoName = arguments[1];        // 使用的算法名称
     carNum = arguments[0] == "" ? vector<int>{} : vector<int>{stoi(arguments[0])};      // 测试集车辆数目
-    // TODO 改一下读入路径和写出路径
     dataPath = "/Users/l/PSP/data";        // 数据输入路径
     outputPath = "/Users/l/PSP/result/" + algoName + "/" ;         // 结果输出路径
     instancePrefix = "data_";  // 实例前缀
     instanceSuffix = ".csv";  // 实例后缀
-    timeLimit = arguments[2] == "" ? INT_MAX : stoi(arguments[1]);  // 求解时间限制
+    timeLimit = arguments[2] == "" ? -1 : stoi(arguments[1]);  // 求解时间限制
 }
